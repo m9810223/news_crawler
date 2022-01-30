73224154e2e5f5762d1dd883bdd3fd64a9a3f6b2
m9810223 <m9810223@gmail.com>
2022-01-30 23:34:21 +0800

    :sparkles: feat(worker.py): add logging

--- src/worker.py
+++ src/worker.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import logging
+from sys import stdout
 
 from crawler import PTT, Job104
 from writer import PTTWriter, Job104Writer
@@ -12,9 +14,19 @@ works = {
 
 class Worker:
     def __init__(self, work_name, folder: Path, keyword,  page):
-        work_name = work_name.lower()
-        crawler, writer = works[work_name]
+        self.work_name = work_name.lower()
+        self.keyword = keyword.lower()
+        crawler, writer = works[self.work_name]
         self.crawler = crawler(keyword, page)
-        self.writer = writer(folder/work_name/keyword)
-        # start
+        self.writer = writer(folder/self.work_name/keyword)
+        self.setup_logger()
+        self.start()
+
+    def setup_logger(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.logger.addHandler(logging.StreamHandler(stdout))
+
+    def start(self):
+        self.logger.debug(f'{self.work_name} -> {self.keyword}')
         self.writer.write(self.crawler())
