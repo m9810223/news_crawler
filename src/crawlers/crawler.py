2b39fd07bbe7e0ca04bd11afa284b05d36f920b8
m9810223 <m9810223@gmail.com>
2022-01-30 19:18:08 +0800

    :recycle: refactor: crawlers

---
+++ src/crawlers/crawler.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from abc import ABC, abstractmethod
+from functools import partial
+from json import loads
+
+from bs4 import BeautifulSoup
+
+REPO_DIR = Path(__file__).resolve().parent.parent
+PUBLIC_DIR = Path(__file__).resolve().parent.parent/'public'
+
+
+class Crawler(ABC):
+    _my_soup = partial(BeautifulSoup, features="html.parser")
+    _my_json_loads = partial(loads)
+
+    def __init__(self, host, init_path, page):
+        self.host = host
+        self.init_path = init_path
+        self.page = page
+
+    def __call__(self):
+        result = []
+        for _ in range(self.page):
+            entries = self.crawl()
+            result.extend(entries)
+        return result
+
+    @abstractmethod
+    def crawl(self):
+        return NotImplemented
